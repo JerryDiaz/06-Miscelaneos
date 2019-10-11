9d4de635f10338f7f0a2045f2ac28f789f2d3e33
Jerry Diaz Gonzalez <jdiaz2040@hotmail.com>
2019-10-11 17:33:56 -0600

    Se agregan componentes

---
+++ src/app/directives/resaltado.directive.ts
@@ -0,0 +1,14 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appResaltado]'
+})
+export class ResaltadoDirective {
+
+  constructor() {
+
+    console.log("Directiva llamada");
+
+   }
+
+}
